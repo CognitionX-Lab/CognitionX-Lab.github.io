ed5119b23a5601c8d65b0e14eb970f634d1d76b1
Usama Jahangir <mr.usama.jahangir@gmail.com>
2025-10-12 10:14:57 +0500

    upload static website

---
+++ out/_next/static/chunks/app/project/[slug]/page-90b0e46ce9470362.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[925,301,214,599],{2037:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=2037)}),_N_E=n.O()}]);
