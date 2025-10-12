c82d1a36712d1469a314af967fa1c19b515233dc
usamajahangir <usamajahangir@users.noreply.github.com>
2025-10-12 05:32:39 +0000

    deploy: 342029532ded411da69563cf29c8cac91d3dfaf5

--- _next/static/chunks/app/team/page-80807f3c0a1cd136.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[599,301,214,925],{2037:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=2037)}),_N_E=n.O()}]);
