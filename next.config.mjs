102de076ecc197bea76e046ab4a15505fce3ca8a
Cognition X Lab <info.cognitionx@gmail.com>
2025-12-29 18:46:25 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true, // This changes /team/slug to /team/slug/index.html
+  trailingSlash: true, 
   images: {
     unoptimized: true,
   },
+  // ONLY add this if your site is NOT at the main domain root
+  // basePath: '/repository-name', 
 };
 
 export default nextConfig;
