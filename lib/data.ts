2f06bb096176cf643b97707f75655a2c0690ff14
Cognition X Lab <info.cognitionx@gmail.com>
2026-04-13 10:22:56 +0800

    Update welcome message for Dr. Christoph P. Sydora

--- lib/data.ts
+++ lib/data.ts
@@ -1036,7 +1036,7 @@ export const news: NewsItem[] = [
   { id: 'n6', date: '18 Dec 2025', title: '📢 Dr. Jinying Xu gave an invited talk "Digital technologies for intelligent carbon management" at the Department of Building and Real Estate, The Hong Kong Polytechnic University.' },
   { id: 'n7', date: '19 Dec 2025', title: '📢 Dr. Jinying Xu gave an invited seminar "Advancing carbon management with ontology-driven data collection" at the Frontier Forum on Smart Construction and Smart City Research at Huazhong University of Science and Technology.' },
   { id: 'n8', date: '12 Jan 2026', title: '🎉 Welcome Mr. Ruiyan Zheng who joins Cognition X Lab, NUS Department of the Built Enviornment as a Research Engineer/PhD Student.' },
-  { id: 'n9', date: '6 Apr 2026', title: '🎉 Dr. Christoph P. Sydora who joins Cognition X Lab,  NUS Department of the Built Enviornment as a Research Fellow.'},
+  { id: 'n9', date: '6 Apr 2026', title: '🎉 Welcome Dr. Christoph P. Sydora who joins Cognition X Lab,  NUS Department of the Built Enviornment as a Research Fellow.'},
 ]
 export const map = {
   lat: 1.2966,
