5f5d9dabd85cdc423423f4f706345992a164f6f9
Cognition X Lab <info.cognitionx@gmail.com>
2026-04-15 18:30:48 +0800

    Add new events and welcome messages to data

--- lib/data.ts
+++ lib/data.ts
@@ -1037,6 +1037,7 @@ export const news: NewsItem[] = [
   { id: 'n7', date: '19 Dec 2025', title: '📢 Dr. Jinying Xu gave an invited seminar "Advancing carbon management with ontology-driven data collection" at the Frontier Forum on Smart Construction and Smart City Research at Huazhong University of Science and Technology.' },
   { id: 'n8', date: '12 Jan 2026', title: '🎉 Welcome Mr. Ruiyan Zheng who joins Cognition X Lab, NUS Department of the Built Enviornment as a Research Engineer/PhD Student.' },
   { id: 'n9', date: '6 Apr 2026', title: '🎉 Welcome Dr. Christoph P. Sydora who joins Cognition X Lab,  NUS Department of the Built Enviornment as a Research Fellow.'},
+  { id: 'n10', date: '13 Apr 2026', title: '📢 Cognition X Lab had a sharing session with the Reasonable Design Lab led by Prof Pieter Herthogs from Department of Architecture, NUS'},
 ]
 export const map = {
   lat: 1.2966,
